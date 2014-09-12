ad8b9af36fb3b2b35d352e248f2532a220a8c3ec
Viktor <vkall@users.noreply.github.com>
2014-09-12 22:18:01 +0300

    8ball index range fix

--- opbot.py
+++ opbot.py
@@ -156,7 +156,7 @@ class OpBot:
 						
         def eightball(self):
 			theMatrix=["It is certain","It is decidedly so","Without a doubt","Yes definitely","You may rely on it","As I see it, yes","Most likely","Outlook good","Yes","Signs point to yes","Reply hazy try again","Ask again later","Better not tell you now","Only in Glorious Pampas","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"]
-			theChosenOne = random.randint(0, len(theMatrix))
+			theChosenOne = random.randint(0, len(theMatrix)-1)
 			self.sendmsg(theMatrix[theChosenOne])
                 
    
